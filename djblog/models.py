ec9fa03f41c8b27be62a974d267022e05708f0a9
SebbieMzingKe <sebbievilar2@gmail.com>
2024-10-27 18:17:16 +0300

    build list and detail views for all posts and individual posts, create templates for views

--- djblog/models.py
+++ djblog/models.py
@@ -30,7 +30,7 @@ class Post(models.Model):
 
     status = models.CharField(
         max_length=2,
-        choices=Status,
+        choices=Status.choices,
         default=Status.DRAFT
     )
 
--- djblog/views.py
+++ djblog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404 ,render
+from .models import Post
+from django.http import  Http404
+
 
 # Create your views here.
+
+
+def post_list(request): # list all poosts
+    posts = Post.published.all()
+    return render(
+        request,
+        '/home/sebbie/Desktop/projects/full-blog/djblog/post/list.html',
+        {'posts':posts}
+    )
+
+# return a single post
+def post_detail(request, id):
+    # try:
+    #     post = Post.published.get(id = id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found")
+
+    post = get_object_or_404(
+        Post,
+        id = id,
+        status = Post.Status.PUBLISHED
+    )
+
+    return render(
+        request,
+        '/home/sebbie/Desktop/projects/full-blog/djblog/templates/post/detail.html',
+        {'post':post}
+    )
